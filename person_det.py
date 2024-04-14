5d273082b974b915342d9c88809c18c0cc793a5a
qwer030413 <comosjay@gmail.com>
2024-04-13 18:38:29 -0700

    object

--- person_det.py
+++ person_det.py
@@ -1,10 +1,12 @@
 import cv2
 import numpy as np
-cap = cv2.VideoCapture(1)
+# change when using different camera
+cap = cv2.VideoCapture(0)
 
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 face_cascade2 = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_profileface.xml')
+fullbody_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_fullbody.xml')
 
 while True:
     ret, frame = cap.read()
@@ -14,7 +16,7 @@ while True:
     rightface = face_cascade2.detectMultiScale(gray_frame,1.3,5)
     flipped = cv2.flip(gray_frame, 1)
     leftface = face_cascade2.detectMultiScale(flipped,1.3,5)
-     
+    fullbody = fullbody_cascade.detectMultiScale(gray_frame,1.15,5)
     for (x, y, a, b) in faces:
         cv2.rectangle(frame, (x, y), (x + a, y + b), (0, 0, 255), 5)
         
@@ -23,13 +25,15 @@ while True:
         cv2.rectangle(frame, (x, y), (x + a, y + b), (0, 0, 255), 5)
        
 
-    print(rightface)        
     
     for (x, y, a, b) in leftface:
         width  = int(cap.get(3)) 
         height = int(cap.get(4))
         cv2.rectangle(frame, (width - x,y), (width - (x + a), y + b), (0, 0, 255), 5)
        
+    for (x, y, a, b) in fullbody:
+        cv2.rectangle(frame, (x, y), (x + a, y + b), (0, 0, 255), 5)
+        
     cv2.imshow('frame', frame)
 
     if cv2.waitKey(1) == ord('q'):
