4e2d49ad73f92781ec046c555dd3b3c12da630fa
qwer030413 <comosjay@gmail.com>
2024-04-13 18:06:59 -0700

    wont work super well if you are lookin at the left

--- person_det.py
+++ person_det.py
@@ -1,28 +1,38 @@
-import numpy as np
 import cv2
-
-cap = cv2.VideoCapture(1)
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+import numpy as np
+cap = cv2.VideoCapture(0)
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+face_cascade2 = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_profileface.xml')
+
+
 
 while True:
     ret, frame = cap.read()
 
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray_frame, 1.3, 6, 0, [30, 30])
-
+    faces = face_cascade.detectMultiScale(gray_frame, 1.3,5)
+    rightface = face_cascade2.detectMultiScale(gray_frame,1.3,5)
+    flipped = cv2.flip(gray_frame,1)
+    leftface = face_cascade2.detectMultiScale(flipped,1.3,5)
+     
     for (x, y, a, b) in faces:
         cv2.rectangle(frame, (x, y), (x + a, y + b), (0, 0, 255), 5)
-        roi_gray = gray_frame[x:x+a, y:y+a]
-        roi_color = frame[x:x+b, y:y+a]
-        eyes = eye_cascade.detectMultiScale(roi_gray, 1.3, 5)
-        for (ex, ey, ea, eb) in eyes:
-            cv2.rectangle(roi_color, (ex, ey), (ex + ea, ey+ eb), (0, 255, 0), 3)
-
+        
+     
+    for (x, y, a, b) in rightface:
+        cv2.rectangle(frame, (x, y), (x + a, y + b), (0, 0, 255), 5)
+       
+            
+    # for (x, y, a, b) in leftface:
+    #     width  = int(cap.get(3)) 
+    #     height = int(cap.get(4))
+    #     cv2.rectangle(frame, (x,y), (width - a, width - b), (0, 0, 255), 5)
+       
     cv2.imshow('frame', frame)
 
     if cv2.waitKey(1) == ord('q'):
         break
 
-cap.release()
+cap.release() # clears usage of video capture
 cv2.destroyAllWindows()
