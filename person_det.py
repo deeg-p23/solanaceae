ea2c0fdc39d244fa18f8a36b14cd9556ee8f7823
deeg-p23 <23diegopereyra@gmail.com>
2024-04-14 08:47:46 -0700

    proof of concept of weapon-detection system
    .csv + .record data was generated based on 76 train images and 10 validation images, though it could not be completed as more time was needed to train the data into .pb files. the object-detection of weapons is setup in person_det.py comments, wouldve been uncommented and working well if a couple more hours was possible qq

--- person_det.py
+++ person_det.py
@@ -8,8 +8,30 @@ face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_fronta
 face_cascade2 = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_profileface.xml')
 fullbody_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_fullbody.xml')
 
+# config_file = 'obj_det models/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+# frozen_model = 'frozen_inference_graph.pb'
+
+# object_model = cv2.dnn_DetectionModel(frozen_model, config_file)
+
+# object_label = ['Mobile phone']
+
+# object_model.setInputSize(320, 320)
+# object_model.setInputScale(1.0/127.5)
+# object_model.setInputScale((127.5, 127,5,127.5))
+# object_model.setInputSwapRB(True)
+
+object_font_scale = 3
+object_font = cv2.FONT_HERSHEY_PLAIN
+
+face_drawn = False
+
+width  = int(cap.get(3)) 
+height = int(cap.get(4))
+
 while True:
     ret, frame = cap.read()
+
+    face_drawn = False
 
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray_frame, 1.3,5)
@@ -17,19 +39,21 @@ while True:
     flipped = cv2.flip(gray_frame, 1)
     leftface = face_cascade2.detectMultiScale(flipped,1.3,5)
     fullbody = fullbody_cascade.detectMultiScale(gray_frame,1.15,5)
+
     for (x, y, a, b) in faces:
         cv2.rectangle(frame, (x, y), (x + a, y + b), (0, 0, 255), 5)
+        face_drawn = True
         
      
     for (x, y, a, b) in rightface:
-        cv2.rectangle(frame, (x, y), (x + a, y + b), (0, 0, 255), 5)
+        if not face_drawn:
+            cv2.rectangle(frame, (x, y), (x + a, y + b), (0, 0, 255), 5)
        
 
-    
     for (x, y, a, b) in leftface:
-        width  = int(cap.get(3)) 
-        height = int(cap.get(4))
-        cv2.rectangle(frame, (width - x,y), (width - (x + a), y + b), (0, 0, 255), 5)
+        if not face_drawn:
+            cv2.rectangle(frame, (width - x,y), (width - (x + a), y + b), (0, 0, 255), 5)
+       
        
     for (x, y, a, b) in fullbody:
         cv2.rectangle(frame, (x, y), (x + a, y + b), (0, 0, 255), 5)
